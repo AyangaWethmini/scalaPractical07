c2bae3b260436f60064a76a7c4faa91e7fb04b62
Ayanga Wethmini <140165014+AyangaWethmini@users.noreply.github.com>
2024-08-06 11:00:47 +0530

    q1

--- q1.scala
+++ q1.scala
@@ -4,9 +4,9 @@ object Q1 extends App {
   // }
 
   def filterEvenNumbers: Array[Int] => Array[Int] = numbers =>
-    numbers.filter(i => i % 2 == 0)
+    numbers.filter(_ % 2 == 0)
 
-  val numArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val numArray = Array(-1, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
   println("Even Numbers : " + filterEvenNumbers(numArray).mkString(", "))
 }
